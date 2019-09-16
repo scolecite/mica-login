af1ad55ebe1a243c4933cdcee0ed72f22eca45ea
kian <kbradley@ucmerced.edu>
2019-09-15 22:58:45 -0400

    start database work

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
-const session = require('express-session');
+const session = require('express-session')
+const sqlite3 = require('sqlite3').verbose()
 const app = express()
 const port = 3000
 
@@ -21,26 +22,25 @@ app.post('/login', function(req, res) {
 	if (req.body.username == 'admin' && req.body.password == 'password') {
 		res.redirect('admin.html')
 	} else {
+		// TODO - perform database insert operation here
 		res.redirect('https://mica.edu')
 	}
+})
+
+// initialize the database. (make a new one if it doesn't exist already)
+console.log("Setting up database")
+let db = new sqlite3.Database('./db/micaphisher.db', sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE, (err) => {
+	if (err) {
+	  console.error(err.message)
+	}
+	console.log('Connected to the micaphisher database.')
+	db.run("create table if not exists micadata(username text, password text);")
 })
 
 // start server
 app.listen(port, () => console.log(`Listening on port ${port}`))
 
-//sqlite
-const sqlite3 = require('sqlite3').verbose();
 
-console.log("setting up database");
-
-let db = new sqlite3.Database('./db/micaphisher.db', (err) => {
-	if (err) {
-	  console.error(err.message);
-	}
-	console.log('Connected to the micaphisher database.');
-  });
-
-
-// create table micadata(username text, password text);
+// create table if not exists micadata(username text, password text);
 // insert into micadate (username, password) values ('chuck@mica.edu', 'kidwalom1@');
 // select * from micadata;
